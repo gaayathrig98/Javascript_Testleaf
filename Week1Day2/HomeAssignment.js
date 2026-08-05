28b0596703ba9a4c962356507d68c2d17be03cd2
Gayathri <gaayathrig98@gmail.com>
2026-08-05 17:04:35 +0530

    Added new files

---
+++ Week1Day2/HomeAssignment.js
@@ -0,0 +1,29 @@
+let userName = "John"
+console.log("User Name is :"  , userName)
+console.log(typeof userName)
+
+let organization = 'XYZ Limited'
+console.log('Organization name is :' , organization)
+console.log(typeof organization)
+
+let contactNumber = 9876543120
+console.log('Contact Number is :' , contactNumber);
+console.log(typeof contactNumber)
+
+let knowsAutomation = true
+console.log("User knows Automation" ,knowsAutomation)
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
+
+
+
+
+
+
