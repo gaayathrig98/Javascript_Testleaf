7f6c8e225e1b0ba257a38cb862d95bc8958478b8
gaayathrig98 <gaayathrig98@gmail.com>
2026-08-05 12:39:45 +0530

    Add files via upload
    Day 1 and 2 uploaded

---
+++ Week1Day2/ClassAss.js
@@ -0,0 +1,19 @@
+var firstName = "John"
+console.log(firstName)
+console.log(typeof firstName)
+
+var companyName =null 
+console.log (companyName)
+console.log (typeof companyName)
+
+var mobileNumber = 9876543210
+console.log (mobileNumber)
+console.log (typeof mobileNumber)
+
+var isAutomation = true
+console.log (isAutomation)
+console.log (typeof isAutomation)
+
+var hasplaywright
+console.log (hasplaywright)
+console.log (typeof hasplaywright)
---
+++ Week1Day3/ClassAss1.js
@@ -0,0 +1,25 @@
+/*
+const browserVersion = "chrome"
+function getbrowserVersion() 
+{
+    if (browserVersion == "chrome")
+         {
+        var browserVersion = "chrome version is 114"
+    }
+console.log('Inside function:', browserVersion)
+}
+getbrowserVersion()
+console.log('Outside function:', browserVersion)
+*/
+
+const browserVersion = "chrome"
+function getbrowserVersion() 
+{
+    if (browserVersion == "chrome")
+         {
+        let browserVersion = "chrome version is 114"
+    }
+console.log('Inside function:', browserVersion)
+}
+getbrowserVersion()
+console.log('Outside function:', browserVersion)
